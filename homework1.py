93e57e1aa864ac39741eb56a3ba00b877fba2200
zura dzadzamia <dbqhvkfg6d@privaterelay.appleid.com>
2026-04-03 17:35:18 -0400

    Add various calculations and print statements

--- homework1.py
+++ homework1.py
@@ -1,1 +1,68 @@
-print(
+Name = "Lola"
+Age = 19
+Height = 176
+is_adult = Age >=18
+
+print(f"Name: {Name}")
+print(f"Age: {Age}")
+print(f"Height: {Height}")
+print(f"is adult: {is_adult}")
+
+
+
+
+
+Price = 150.0
+Price_with_taxes = Price * 1.12
+Discount = Price - 30.0
+After_split = Price / 3
+Price_per_person = After_split
+
+print(Price)
+print(Price_with_taxes)
+print(Discount)
+print(After_split)
+print(Price_per_person)
+
+
+
+
+
+temperature = 38.5
+normal_temperature = 36.6
+Greater = temperature > normal_temperature
+Equal = temperature == normal_temperature
+Not_equal = temperature != normal_temperature
+Less = temperature < normal_temperature
+
+print(f"Temp is greater than normal temp: {Greater}")
+print(f"Temp is equal to normal temp: {Equal}")
+print(f"Temp is not equal to normal temp: {Not_equal}")
+print(f"Temp is less than normal temp: {Less}")
+
+
+
+
+a = 42
+b = 3.14
+c = "hello"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+
+
+x = 17
+y = 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
